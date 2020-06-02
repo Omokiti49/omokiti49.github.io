421d1eabae40b0222e8f709f32227ba594fadff0
Omokiti49 <65245891+Omokiti49@users.noreply.github.com>
2020-06-01 23:33:20 -0400

    Update Assignment3.js

--- Assignment3.js
+++ Assignment3.js
@@ -143,13 +143,14 @@ elem18.children[4].children[1].onclick
   = function(){subtract1(elem18);};
 elem18.children[4].children[2].onclick 
   = function(){clear(elem18);};
-// you don't have to define the function before you use it! 
+
 function add1 (elem) {
   if(elem.children[2].innerHTML == "-") elem.children[2].innerHTML = "1";
   else {
     let currentScore = elem.children[2].innerHTML;
     currentScore = Number.parseInt(currentScore);
     elem.children[2].innerHTML = currentScore + 1;
+    elem.children[3].innerHTML = currentScore + 1;
   }
 }
 
@@ -159,5 +160,6 @@ function subtract1 (elem) {
     let currentScore = elem.children[2].innerHTML;
     currentScore = Number.parseInt(currentScore);
     elem.children[2].innerHTML = currentScore - 1;
+    elem.children[3].innerHTML = currentScore - 1;
   }
 }
