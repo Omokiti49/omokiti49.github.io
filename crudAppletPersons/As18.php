4dab10b4e5425b911735bdf0d947815ff5f5bab3
Omokiti49 <65245891+Omokiti49@users.noreply.github.com>
2021-04-10 22:39:40 -0400

    Update As18.php

--- crudAppletPersons/As18.php
+++ crudAppletPersons/As18.php
@@ -7,65 +7,13 @@ function main () {
 	$obj = json_decode($json_string);
 	
 	$death_arr = Array();
+		$arr2 = Array();
 	foreach($obj->Countries as $i){
-		array_push[$i->Country, $i->TotalDeaths])
-		/*$data = $obj->Countries[$i]->Country . " : "
-			. $obj->Countries[$i]->TotalDeaths;
-		echo = $i->Country . " : "
-			. $i->TotalDeaths
-			. "<br>";*/
+		array_push($death_arr, $i->Country);
+		array_push($arr2, $i->$i->TotalDeaths);	
 	}
-	/*foreach($obj->Countries as $i){
-
-	}
-
-		echo $data . " <br><br> ";*/
-	// echo html head section
-	//echo '<html>';
-	//echo '<head>';
-	//echo '	<link rel="icon" href="img/cardinal_logo.png" type="image/png" />';
-	//echo '</head>';
-	
-	// open html body section
-	//echo '<body onload="loadDoc()">';
-	
-	//echo '<div>';
-	//$myObjString = '{"newCases1":' . $data . '}' ;
-	//echo $myObjString;
-	//echo '</div>';
-	
-	//echo '<div>';
-	//$myArray = array("newCases2"=>$data) ;
-	// $myArray = array("newCases2"=>array("A"=>1, "B"=>2)) ;
-	//echo json_encode($myArray);
-	//echo '</div>';
-	
-	//echo '<div id="demo">';
-	//echo '</div>';
-	//echo '<script>';
-	//echo '
-	/*	var country_usa;
-		function loadDoc() {
-		  var xhttp = new XMLHttpRequest();
-		  xhttp.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-			  country_usa = JSON.parse(this.responseText).Global.NewConfirmed;
-			  document.getElementById("demo").innerHTML = country_usa;
-			 
-			}
-		  };
-		  var api = "https://api.covid19api.com/summary";
-		  xhttp.open("GET", api, true);
-		  xhttp.send();
-		}
-	';
-	echo '</script>';
-	
-	// close html body section
-	echo '</body>';
-	echo '</html>';
-}*/
-
+		array_multisort($arr2, $death_arr);
+		print_r($death_arr);
 
 #-----------------------------------------------------------------------------
 // read data from a URL into a string
